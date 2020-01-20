623cfe27453484435a8f457b6491a12f7e7a6654
Morphage <4647675+Morphage@users.noreply.github.com>
2020-01-20 15:07:41 +0000

    Finished courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,40 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
-    return <h1>{props.course}</h1>
+    return <h1>{props.name}</h1>
 }
 
 const Part = (props) => {
-    return <p>{props.name} {props.numberOfExercises}</p>
+    return <p>{props.name} {props.exercises}</p>
 }
 
 const Content = (props) => {
     return props.parts.map(part => (
-        <Part name={part.name} numberOfExercises={part.numberOfExercises} />
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
     ))
 }
 
 const Total = (props) => {
-    const total = props.parts.reduce((a, b) => a + b.numberOfExercises, 0)
+    const total = props.parts.reduce((a, b) => a + b.exercises, 0)
     return <p>Number of exercises {total}</p>
 }
 
-const App = () => {
-    const course = 'Half Stack application development'
-
-    const parts = [
-        { name: 'Fundamentals of React', numberOfExercises: 10 },
-        { name: 'Using props to pass data', numberOfExercises: 7 },
-        { name: 'State of a component', numberOfExercises: 14 }
-    ]
-
+const Course = (props) => {
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts}/>
+            <Header name={props.course.name} />
+            <Content parts={props.course.parts} />
+            <Total parts={props.course.parts} />
         </div>
     )
+}
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            { name: 'Fundamentals of React', exercises: 10 },
+            { name: 'Using props to pass data', exercises: 7 },
+            { name: 'State of a component', exercises: 14 }
+        ]
+    }
+
+    return <Course course={course} />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
