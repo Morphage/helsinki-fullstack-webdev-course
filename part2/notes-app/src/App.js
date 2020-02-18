174ddbae270cc17efbfaead163198c511e3630d3
Morphage <4647675+Morphage@users.noreply.github.com>
2020-02-18 11:19:14 +0000

    Add code to notes-app

--- part2/notes-app/src/App.js
+++ part2/notes-app/src/App.js
@@ -1,34 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
+import noteService from './services/notes'
 
-const App = (props) => {
-    const [notes, setNotes] = useState(props.notes)
-    const [newNote, setNewNote] = useState(
-        'a new note...'
-    )
+const App = () => {
+    const [notes, setNotes] = useState([])
+    const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
 
-    const notesToShow = showAll ? notes : notes.filter(note => note.isImportant)
+    useEffect(() => {
+        noteService
+            .getAll()
+            .then(initialNotes => {
+                setNotes(initialNotes)
+            })
+    }, [])
+
+    const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+    const toggleImportanceOf = (id) => {
+        const note = notes.find(n => n.id === id)
+        const changedNote = { ...note, important: !note.important }
+
+        noteService
+        .update(id, changedNote)
+            .then(returnedNote => {
+                setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+            })
+    }
 
     const rows = () => notesToShow.map(note =>
         <Note
             key={note.id}
             note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
         />
     )
 
     const addNote = (event) => {
         event.preventDefault()
-        
         const noteObject = {
             content: newNote,
-            date: new Date().toISOString(),
-            isImportant: Math.random() > 0.5,
-            id: notes.length + 1
+            date: new Date(),
+            important: Math.random() > 0.5,
         }
 
-        setNotes(notes.concat(noteObject))
-        setNewNote('')
+        noteService
+            .create(noteObject)
+            .then(returnedNote => {
+                setNotes(notes.concat(returnedNote))
+                setNewNote('')
+            })
     }
 
     const handleNoteChange = (event) => {
@@ -41,14 +62,14 @@ const App = (props) => {
             <h1>Notes</h1>
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
-                    show { showAll ? 'important' : 'all' }
+                    show {showAll ? 'important' : 'all'}
                 </button>
             </div>
             <ul>
                 {rows()}
             </ul>
             <form onSubmit={addNote}>
-                <input 
+                <input
                     value={newNote}
                     onChange={handleNoteChange}
                 />
--- part2/notes-app/src/index.js
+++ part2/notes-app/src/index.js
@@ -2,28 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-const notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2019-05-30T17:30:31.098Z',
-        isImportant: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only Javascript',
-        date: '2019-05-30T18:39:34.091Z',
-        isImportant: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2019-05-30T19:20:14.298Z',
-        isImportant: true
-    }
-]
-
 ReactDOM.render(
-    <App notes={notes} />,
+    <App />,
     document.getElementById('root')
 )
