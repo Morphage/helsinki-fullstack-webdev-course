74f5922cc1d9c8fd26c1f6fd21e742d7c3838e45
Morphage <4647675+Morphage@users.noreply.github.com>
2020-02-27 19:05:44 +0000

    Part 3 - notes backend and phonebook backend exercises

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (_, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (_, res) => {
+    const n = persons.length
+
+    res.send(
+        `<div>Phonebook has info for ${n} people</div><div>${new Date()}</div>`
+    )
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    person ? res.json(person) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 99999) + 1000
+}
+
+const isAlreadyInPhonebook = (name) => {
+    return persons.filter(p => p.name === name).length > 0
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    const { name, number } = body
+    if (!name || !number) {
+        return response.status(400).json({
+            error: 'name or number field is missing'
+        })
+    }
+
+    if (isAlreadyInPhonebook(name)) {
+        return response.status(409).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name,
+        number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
