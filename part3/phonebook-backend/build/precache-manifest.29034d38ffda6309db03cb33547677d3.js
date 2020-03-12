83ba37dabd2b36287b3f5a6817a268568c115d2e
Morphage <4647675+Morphage@users.noreply.github.com>
2020-03-12 18:20:21 +0000

    Finished phonebook exercises part 3b

---
+++ part3/phonebook-backend/build/precache-manifest.29034d38ffda6309db03cb33547677d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8206d7474724509ea1d7f941642571b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20f7a65650c00e85a885",
+    "url": "/static/css/main.30d64dcd.chunk.css"
+  },
+  {
+    "revision": "caa7b2c25ff02c9abe02",
+    "url": "/static/js/2.d7346c78.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d7346c78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20f7a65650c00e85a885",
+    "url": "/static/js/main.5cbec52d.chunk.js"
+  },
+  {
+    "revision": "73cc5c71f716e545cadb",
+    "url": "/static/js/runtime-main.a70bf60b.js"
+  }
+]);
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestBody'))
 morgan.token('requestBody', function (req, _) { return JSON.stringify(req.body) })
 
+app.use(express.static('build'))
 app.use(cors())
 
 let persons = [
