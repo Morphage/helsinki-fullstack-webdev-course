bd25f8ddbff9f201fc4e5d2ebdf173d3c560f4d4
Morphage <4647675+Morphage@users.noreply.github.com>
2020-04-27 18:44:51 +0100

    Notes backend for part4

---
+++ part4/notes-backend/build/precache-manifest.e449160982363a136b0c491782b84c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0746f9bce73f2034796b31d4c956cf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8021ca9c387c92ecf19",
+    "url": "/static/css/main.19c2ecc7.chunk.css"
+  },
+  {
+    "revision": "7a4c961065e71cad2dcd",
+    "url": "/static/js/2.2b296be9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2b296be9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8021ca9c387c92ecf19",
+    "url": "/static/js/main.22511305.chunk.js"
+  },
+  {
+    "revision": "8eb86d135bc80d76d217",
+    "url": "/static/js/runtime-main.afb0aeba.js"
+  }
+]);
