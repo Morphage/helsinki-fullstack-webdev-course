23d8453d1fb4194487911759c80c4ca80dcf4ee8
Morphage <4647675+Morphage@users.noreply.github.com>
2020-07-13 18:37:33 +0100

    Finished exercise 4.16

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/users', async (request, response) => {
+  const body = request.body
+  const username = body.username
+  const password = body.password
+
+  if (!username || !password) {
+    return response.status(400).json(
+      { error: 'username or password is missing' }
+    )
+  }
+
+  if (username.length < 3 || password.length < 3) {
+    return response.status(400).json(
+      { error: 'username or password must be at least 3 characters long' }
+    )
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.json(savedUser)
+})
+
+usersRouter.get('/users', async (request, response) => {
+  const users = await User.find({})
+  response.json(users.map(u => u.toJSON()))
+})
+
+module.exports = usersRouter
